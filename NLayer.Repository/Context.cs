697151986e64ae4a7553365b3dd1f70fb9c2b768
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-10-11 22:12:13 +0300

    NLayer Projesinde Katmanlar Oluşturuldu. Core katmanı tamamlandı.

---
+++ NLayer.Repository/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+	public class Context : DbContext
+	{
+        public Context(DbContextOptions<Context> options):base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+
+
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
